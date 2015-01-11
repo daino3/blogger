6dfcadd8476bd633f36fb433953eed43b09b64c9
daino3 <dainovu3@gmail.com>
2015-01-10 18:04:51 -0600

    Adding asset precompiling to deploy script

--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -1,10 +1,11 @@
 path = ''
 
-if Rails.env.production?
-  path = Rails.root.join('../../', 'smtp_settings.yml')
-else
-  path = Rails.root.join('config', 'smtp_settings.yml')
-end
+path =  if Rails.env.production?
+          Rails.root.join('../', 'smtp_settings.yml')
+        else
+          Rails.root.join('config', 'smtp_settings.yml')
+        end
+
 file = YAML.load(File.read(path))
 
 file.each do |key, value|
@@ -14,7 +15,7 @@ end
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => "railscasts.com",
+  :domain               => "gmail.com",
   :user_name            => ENV["user_name"],
   :password             => ENV["password"],
   :authentication       => "plain",
