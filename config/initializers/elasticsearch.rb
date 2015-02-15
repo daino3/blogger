7460c72e730f469ea3e84551c68cbdc4a7dc3ad0
daino3 <dainovu3@gmail.com>
2015-02-15 14:17:19 -0600

    adding elasticsearch yml config

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,15 +1,13 @@
-es_file = File.join(Rails.root, 'config', 'elasticsearch.yml')
+es_file = ''
+# Point to feeds server
+if Rails.env.production?
+  es_file = Rails.root.join('..','shared', 'elasticsearch.yml')
+else
+  es_file = Rails.root.join('..', 'elasticsearch.yml')
+end
 
-YAML.load(File.open(es_file)).each do |key, value|
+YAML.load_file(es_file)[Rails.env].each do |key, value|
   ENV[key.to_s] = value
 end
 
-# Point to feeds server
-case Rails.env
-  when 'staging'
-    Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['ES_STAGING_URL'], logs: true})
-  when 'production'
-    Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['ES_PRODUCTION_URL'], logs: true})
-  else
-    Elasticsearch::Model.client = Elasticsearch::Client.new({url: 'http://localhost:9200', logs: true})
-end
+Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['elastic_search_url'], logs: true})
