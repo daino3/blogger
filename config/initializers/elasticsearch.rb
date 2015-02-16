6c7c280e34a189820b10aee92a89109ab13bb1de
daino3 <dainovu3@gmail.com>
2015-02-16 17:50:09 -0600

    Changing elasticsearch yml location... again

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,10 +1,11 @@
-es_file = ''
-# Point to feeds server
-if Rails.env.production?
-  es_file = Rails.root.join('../..','shared', 'elasticsearch.yml')
-else
-  es_file = Rails.root.join('..', 'shared', 'elasticsearch.yml')
-end
+production_location = Rails.root.join('../..','shared', 'elasticsearch.yml')
+local_location = Rails.root.join('..', 'elasticsearch.yml')
+
+es_file = if File.exists?(production_location)
+            production_location
+          else
+            local_location
+          end
 
 YAML.load_file(es_file)[Rails.env].each do |key, value|
   ENV[key.to_s] = value
