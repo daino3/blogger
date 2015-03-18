58fee5618394ddb85c030902ae34a92df0dfe915
daino3 <dainovu3@gmail.com>
2015-03-17 20:34:23 -0500

    Updated resume - God, I wrote this poorly

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -4,5 +4,9 @@ module ImageHelper
     stripped_url = url.gsub(/.*\/images\//, "")
     image_tag(stripped_url)
   end
+
+  def resume_path()
+    "/assets/#{Photo.find_by_title("resume").get_url(type: "original")}"
+  end
 end
 
