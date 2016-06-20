d013254c80124468febb026bbae3d2925e845761
daino3 <dainovu3@gmail.com>
2016-06-19 22:31:46 -0500

    swapped out resume

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,12 +1,10 @@
 module ImageHelper
-
   def image_link(url)
     stripped_url = url.gsub(/.*\/images\//, "")
     image_tag(stripped_url)
   end
 
-  def resume_path()
-    "/assets/#{Photo.find_by_title("resume").get_url(type: "original")}"
+  def resume_path
+    "/assets/uploads/original/resume.pdf"
   end
 end
-
